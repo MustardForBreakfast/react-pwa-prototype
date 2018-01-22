c18543ad958f0c8f1fcb9bc11788ab82f76087ec
Justin Wayne <justin.wayne@springbox.com>
2018-01-22 14:21:30 -0600

    organization

--- client/src/js/main.jsx
+++ client/src/js/main.jsx
@@ -4,14 +4,13 @@ import App from './App';
 
 // include styles here so webpack can find 'em and bundle 'em up
 import '../scss/main.scss';
-import './vendor.js'; // vendor css imported here.
+import './vendor.js'; // vendor css is referenced here.
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js');
+}
 
 ReactDOM.render(
   (<App />),
   document.getElementById('root'),
 );
-
-// register service worker, if supported
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js');
-}
